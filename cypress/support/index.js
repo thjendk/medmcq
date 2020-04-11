3018acdbc72a3d2ef965b972022a79957a0270a9
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2020-04-11 17:33:44 +0200

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -73,7 +73,7 @@ describe('authentication', () => {
       cy.get('Input[placeholder=Kodeord]').type('Password1');
       cy.get('Input[placeholder="Gentag kodeord"]').type('Password1');
       cy.wait(1000); // Username and email is being checked
-      cy.contains('button', 'Opret').click();
+      cy.contains('button', 'Opret bruger').click();
       cy.url().should('not.include', 'opret');
       cy.getCookie('user').should('be.ok');
     });
@@ -105,7 +105,7 @@ describe('authentication', () => {
       cy.get('Input[placeholder=Kodeord]').type('Password2');
       cy.get('Input[placeholder="Gentag kodeord"]').type('Password2');
       cy.wait(1000); // Username and email is being checked
-      cy.contains('button', 'Opret').click();
+      cy.contains('button', 'Opret bruger').click();
       cy.url().should('not.include', 'opret');
       cy.getCookie('user').should('be.ok');
       cy.logout();
