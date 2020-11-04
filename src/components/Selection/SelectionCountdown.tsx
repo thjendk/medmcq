34d376744d290f0e27e7e460d92a69c271d5075f
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2020-11-04 09:22:13 +0100

    Logger integrated

--- src/components/Selection/SelectionCountdown.tsx
+++ src/components/Selection/SelectionCountdown.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import Countdown from 'react-countdown';
 import { Button, Divider, Grid, Icon, Image, Message } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ReduxState } from 'redux/reducers';
 import image from './tj_profile.jpg';
+import settingsReducer from 'redux/reducers/settings';
+import Log from 'classes/log.class';
 
 interface SelectionCountdownProps {}
 
 const SelectionCountdown = (props: SelectionCountdownProps) => {
+  const dispatch = useDispatch();
+  const hasVoted = useSelector((state: ReduxState) => state.settings.hasVoted);
   const user = useSelector((state: ReduxState) => state.auth.user);
 
+  const openVoting = async (name: string) => {
+    dispatch(settingsReducer.actions.toggleHasVoted());
+    await Log.create({ name });
+  };
+
+  if (hasVoted)
+    return (
+      <Button
+        basic
+        color="green"
+        fluid
+        onClick={() => {
+          window.open('https://e-vote.dk/e-valg-afstemning/faces/Afstemning', '_blank');
+          openVoting('voted again');
+        }}
+      >
+        Tak for din stemme! <Icon name="heart outline" />
+        Fik du ikke stemt? Tryk her.
+      </Button>
+    );
   return (
     <Message style={{ border: '2px solid red' }}>
       <div style={{ textAlign: 'center', fontSize: '2em', color: 'red', fontWeight: 'bolder' }}>
@@ -26,17 +50,21 @@ const SelectionCountdown = (props: SelectionCountdownProps) => {
         <Grid.Column verticalAlign="middle" width="4">
           <Grid.Row>
             <Image src={image} size="medium" />
-            <p style={{ textAlign: 'center' }}>Thomas Jensen, udvikler af MedMCQ</p>
+            <p style={{ textAlign: 'center' }}>
+              Thomas Jensen (Thjen)
+              <br />
+              Udvikler af MedMCQ
+            </p>
           </Grid.Row>
         </Grid.Column>
         <Grid.Column>
           <Grid.Row verticalAlign="middle">
             <p>
-              De sidste 3 år har jeg repræsenteret medicin i studienævnet, og har gennem denne
-              indflydelse blandt andet sikret, at MedMCQ er frit tilgængeligt for alle studerende og
-              løbende får de nyeste eksamenssæt lagt ind. Det er blot én af de ting, som jeg og
-              medicinerrådet har sikret de seneste år i studienævnet, og du kan finde langt mere
-              information på vores{' '}
+              Jeg hedder Thomas Jensen (også kendt her på siden som Thjen). De sidste 3 år har jeg
+              repræsenteret medicin i studienævnet, og har gennem denne indflydelse blandt andet
+              sikret, at MedMCQ er frit tilgængeligt for alle studerende og løbende får de nyeste
+              eksamenssæt lagt ind. Men det er blot en lille del af vores samlede indsats, og du kan
+              finde langt mere information på vores{' '}
               <a href="https://www.facebook.com/MedicinerraadAU" target="__blank">
                 facebook side
               </a>
@@ -44,35 +72,37 @@ const SelectionCountdown = (props: SelectionCountdownProps) => {
             </p>
             <h2 style={{ textAlign: 'center' }}>Der er normalt fredsvalg, men ikke i år.</h2>
             <p>
-              Vi fører næsten aldrig valgkamp, men konservative studenter er i år opposition til
-              medicinerrådet. De er et bred politisk parti, og har ikke kontakt til de studerende på
-              medicin.{' '}
-              <span style={{ color: 'red' }}>
-                Konservative studenter går ind for mindre kvote 2 optagelser, lavere SU, omlægning
-                af alt SU til lån og færre studiepladser.{' '}
-              </span>
-              Det ønsker vi ikke. Medicinerrådet er upolitisk, men er stærkt imod deres politik.
-              Medicinerrådet har hårdtarbejdende semesterrepræsentanter, som sikrer at vi er i
-              løbende dialog med alle studerende og undervisere. Vi har en åben email tilknyttet en
-              fast sekretær, hvor I altid kan komme i kontakt med os, og vi svarer altid på vores{' '}
+              Vi fører næsten aldrig valgkamp, men andre partier vil i år gøre valget politisk i
+              stedet for studenter orienteret. Vores opposition er et bredt politisk parti, som ikke
+              er i kontakt med os på medicin.{' '}
+            </p>
+            <p style={{ color: 'red', textAlign: 'center' }}>
+              De går ind for lavere SU, omlægning af alt SU til lån, færre studiepladser og mindre
+              kvote 2 optagelser.
+            </p>
+            <p>
+              Dette er vi stærkt imod. Medicinerrådet er skabt af studerende for studerende. Vi har
+              hårdtarbejdende semesterrepræsentanter, som sikrer at vi er i løbende dialog med alle
+              studerende og undervisere. Vi har representanter i alle uddannelsesansvarlige nævn på
+              universitet. Derudover har vi en åben email tilknyttet en fast sekretær, hvor I altid
+              kan komme i kontakt med os, og vi svarer altid på vores{' '}
               <a href="https://www.facebook.com/MedicinerraadAU" target="__blank">
                 facebook side
               </a>
               .
             </p>
             <p>
-              Stem på mig (Thomas Jensen, som nogle her på siden nok kender som Thjen) eller en
-              anden repræsentant for medicinerrådet til universitetsvalget for at sikre din
-              indflydelse på uddannelsen. Får medicinerrådet samlet 6 gange så mange stemmer som
-              konservative studenter, holdes de ude af studienævnet.
+              Stem på mig eller på medicinerrådet til universitetsvalget for at sikre de
+              medicinstuderendes inflydelse på deres egen uddannelse. Får medicinerrådet samlet 6
+              gange så mange stemmer som de andre partier, kan vi med flertal i studienævnet sørge
+              for at ændringer kun foretages til de studerendes fordel.
             </p>
-            <h2 style={{ textAlign: 'center' }}>
-              Din stemme er meget vigtig! Det tager max 30 sekunder!
-            </h2>
+            <h2 style={{ textAlign: 'center' }}>Din stemme er meget vigtig!</h2>
             <Button
-              onClick={() =>
-                window.open('https://e-vote.dk/e-valg-afstemning/faces/Afstemning', '_blank')
-              }
+              onClick={() => {
+                window.open('https://e-vote.dk/e-valg-afstemning/faces/Afstemning', '_blank');
+                openVoting('voted');
+              }}
               fluid
               color="green"
               style={{ cursor: 'pointer' }}
