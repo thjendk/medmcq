72019c8d8f60f86df313a46dac2b832b5bf8d002
Thomas Jensen <44089656+thjendk@users.noreply.github.com>
2021-05-04 17:13:00 +0200

    Cleanup

--- src/components/Selection/SelectionRandom.tsx
+++ src/components/Selection/SelectionRandom.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import SelectionNSelector from './SelectionComponents/SelectionNSelector';
 import { Divider } from 'semantic-ui-react';
 import SelectionSearch from './SelectionComponents/SelectionSearch';
-import { useSelector } from 'react-redux';
-import { ReduxState } from 'redux/reducers';
 
 export interface SelectionRandomProps {}
 
