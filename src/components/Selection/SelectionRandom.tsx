50474de355789b491b09e513ec719ea213db688a
Thomas Jensen <44089656+thjendk@users.noreply.github.com>
2021-05-05 15:47:47 +0200

    Search function reintegration (#212)

--- src/components/Selection/SelectionRandom.tsx
+++ src/components/Selection/SelectionRandom.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import SelectionNSelector from './SelectionComponents/SelectionNSelector';
 import { Divider } from 'semantic-ui-react';
 import SelectionSearch from './SelectionComponents/SelectionSearch';
-import { useSelector } from 'react-redux';
-import { ReduxState } from 'redux/reducers';
 
 export interface SelectionRandomProps {}
 
 const SelectionRandom: React.FC<SelectionRandomProps> = () => {
-  const user = useSelector((state: ReduxState) => state.auth.user);
-
   return (
     <div>
       <SelectionNSelector />
-      {user?.role.id <= 3 && (
-        <>
-          <Divider />
-          <SelectionSearch />
-        </>
-      )}
+      <Divider />
+      <SelectionSearch />
     </div>
   );
 };
